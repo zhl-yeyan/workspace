f62b5768378c5e2bdce115db3823b03e384916f3
Zhenlin Zhao <zlzhao@haitaiinc.com>
2025-11-09 21:38:00 +0800

    增加根据当前时区获取时间的mcp

--- spring-ai-cdr/mcp-server/src/main/java/com/zhl/com/zhl/mcp/tool/DateTool.java
+++ spring-ai-cdr/mcp-server/src/main/java/com/zhl/com/zhl/mcp/tool/DateTool.java
@@ -5,6 +5,8 @@ import org.springframework.ai.tool.annotation.Tool;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 
 @Component
@@ -16,6 +18,17 @@ public class DateTool {
         String currentTime = String.format("当前的时间是 %s",
                 LocalDateTime.now().format(
                         DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
+        return currentTime;
+    }
+    @Tool(description = "根据城市所在的时区id来获得当前的时间")
+    public String getCurrentTimeByZoneId(String cityName,String zoneId){
+        log.info("===============调用MCP工具：getCurrentTimeByZoneId===============");
+        log.info(String.format("===============参数 cityName：%s ================================",cityName));
+        log.info(String.format("===============参数 zoneId：%s ================================",zoneId));
+        ZoneId zone = ZoneId.of(zoneId);
+        ZonedDateTime now = ZonedDateTime.now(zone);
+        String currentTime = String.format("当前时间是 %s",
+                now.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
         return currentTime;
     }
 }
